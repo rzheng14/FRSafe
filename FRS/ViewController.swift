1619c094b0a15b679d3bd1987547a5d8589cffe5
Richard Zheng <rzheng14@binghamton.edu>
2019-11-12 16:47:02 -0500

    Initial Commit

---
+++ FRS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FRS
+//
+//  Created by Richard Zheng on 11/12/19.
+//  Copyright © 2019 Richard Zheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
