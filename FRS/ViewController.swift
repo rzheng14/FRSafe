5522750c559d0b6ee4f11965f39e0ef6f4bd7df8
Richard Zheng <rzheng14@binghamton.edu>
2019-11-15 01:13:18 -0500

    camera works, picture doesn't save

--- FRS/ViewController.swift
+++ FRS/ViewController.swift
@@ -7,14 +7,96 @@
 //
 
 import UIKit
+import AVFoundation
+
+
+
 
 class ViewController: UIViewController {
-
+  @IBOutlet weak var previewView: UIView!
+  @IBOutlet weak var snap: UIButton!
+  
+  var captureSession: AVCaptureSession?
+  var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+  
+  var capturePhotoOutput: AVCapturePhotoOutput?
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    snap.layer.cornerRadius = snap.frame.size.width / 2
+    snap.clipsToBounds = true
+    
+    // Sets to front facing camera.
+    guard let captureDevice = AVCaptureDevice.default(.builtInTrueDepthCamera, for: .video,
+                                                      position: .front)
+      else { fatalError("whoops") }
+    
+    do {
+      let input = try AVCaptureDeviceInput(device: captureDevice)
+      
+      captureSession = AVCaptureSession()
+      captureSession?.addInput(input)
+      
+      videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+      videoPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+      videoPreviewLayer?.frame = view.layer.bounds
+      previewView.layer.addSublayer(videoPreviewLayer!)
+      
+      
+      
+      // Get an instance of ACCapturePhotoOutput class
+      capturePhotoOutput = AVCapturePhotoOutput()
+      capturePhotoOutput?.isHighResolutionCaptureEnabled = true
+      captureSession?.addOutput(capturePhotoOutput!)
+
+      
+      captureSession?.startRunning()
+    } catch {
+        print(error)
+        return
+    }
   }
-
-
+  
+  @IBAction func takePic(_ sender: Any) {
+    guard let capturePhotoOutput = self.capturePhotoOutput else { return }
+    
+    
+    let photoSettings = AVCapturePhotoSettings()
+    
+    //still images:
+    photoSettings.isAutoStillImageStabilizationEnabled = true
+    //photoSettings.isAutoStillImageStabilizationEnabled = self.capturePhotoOutput!.isStillImageStabilizationSupported
+    photoSettings.isHighResolutionPhotoEnabled = true
+    photoSettings.flashMode = .auto
+    
+    capturePhotoOutput.capturePhoto(with: photoSettings, delegate: self) //needs the extension below to work
+  }
+  
 }
 
+extension ViewController : AVCapturePhotoCaptureDelegate {
+    func photoOutput(_ captureOutput: AVCapturePhotoOutput,
+                 didFinishProcessingPhoto photoSampleBuffer: CMSampleBuffer?,
+                 previewPhoto previewPhotoSampleBuffer: CMSampleBuffer?,
+                 resolvedSettings: AVCaptureResolvedPhotoSettings,
+                 bracketSettings: AVCaptureBracketedStillImageSettings?,
+                 error: Error?) {
+        // if we dont have a photo buffer
+        guard error == nil,
+            let photoSampleBuffer = photoSampleBuffer else {
+            print("Error capturing photo: \(String(describing: error))")
+            return
+        }
+        
+        guard let imageData = AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: photoSampleBuffer, previewPhotoSampleBuffer: previewPhotoSampleBuffer) else {
+            return
+        }
+        
+        let capturedImage = UIImage.init(data: imageData , scale: 1.0)
+        if let image = capturedImage {
+            // Saves
+            UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        }
+    }
+}
